6c7d9c94f7a18e57a8969dfa2557bbf980bdd939
snitheesh2005-a11y <snitheesh2005@gmail.com>
2026-01-24 20:52:22 +0530

    first commit

---
+++ png.py
@@ -0,0 +1,34 @@
+import numpy as np
+from PIL import Image
+
+# ---------------- CONFIG ----------------
+HEX_FILE = "edge_out5.hex"   # input from RTL
+OUT_IMG  = "output8.png"
+
+IMG_W = 256
+IMG_H = 256
+
+#borders are not ignored
+VALID_W = IMG_W 
+VALID_H = IMG_H 
+VALID_PIXELS = VALID_W * VALID_H
+
+
+# Read hex values
+with open(HEX_FILE, "r") as f:
+    pixels = [int(line.strip(), 16) for line in f if line.strip()]
+
+print("Total pixels in hex file:", len(pixels))
+
+# Take only valid region
+pixels = pixels[:VALID_PIXELS]
+
+# Convert to numpy image
+img = np.array(pixels, dtype=np.uint8)
+img = img.reshape((VALID_H, VALID_W))
+
+# Save cropped image
+Image.fromarray(img, mode="L").save(OUT_IMG)
+
+print(f"✅ PNG generated: {OUT_IMG}")
+print(f"Image size: {VALID_W} x {VALID_H}")
